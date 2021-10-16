7d7b43f30951b745a92a54fcdc27826e3d6d11f9
IGOR TOPAL <mr.topal99@yandex.ru>
2021-10-16 17:37:35 +0300

    fix/bikes-select

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -9,6 +9,8 @@ new Swiper('.bikes__slider', {
 
   spaceBetween: 18,
 
+  observer: true,
+
   autoplay: {
     delay: 5000
   },
@@ -39,3 +41,52 @@ new Swiper('.coatings__slider', {
   slidesPerView: 1,
   allowTouchMove: false,
 });
+
+const select = document.querySelector(".bikes__select");
+const highwayTemplate = document.querySelector("#highway");
+const gravelTemplate = document.querySelector("#gravel");
+const ttTemplate = document.querySelector("#tt");
+const bikesSliderWrapper = document.querySelector(".bikes__slider-wrapper");
+const desctopHighwayButton = document.querySelector("#desc-highway-button");
+const desctopGravelButton = document.querySelector("#desc-gravel-button");
+const desctopTtButton = document.querySelector("#desc-tt-button");
+
+console.log(bikesSliderWrapper.content);
+
+select.addEventListener('change', function() {
+  if(this.value === 'highway') {
+    bikesSliderWrapper.innerHTML = "";
+    bikesSliderWrapper.append(highwayTemplate.content.cloneNode(true));
+  } else if(this.value === 'gravel') {
+    bikesSliderWrapper.innerHTML = "";
+    bikesSliderWrapper.append(gravelTemplate.content.cloneNode(true));
+  } else {
+    bikesSliderWrapper.innerHTML = "";
+    bikesSliderWrapper.append(ttTemplate.content.cloneNode(true));
+  }
+});
+
+desctopHighwayButton.addEventListener("click", function(){
+  bikesSliderWrapper.innerHTML = "";
+  bikesSliderWrapper.append(highwayTemplate.content.cloneNode(true));
+  desctopGravelButton.classList.remove('bikes__nav-element_active');
+  desctopTtButton.classList.remove('bikes__nav-element_active');
+  desctopHighwayButton.classList.add('bikes__nav-element_active');
+});
+
+desctopGravelButton.addEventListener("click", function(){
+  bikesSliderWrapper.innerHTML = "";
+  bikesSliderWrapper.append(gravelTemplate.content.cloneNode(true));
+  desctopTtButton.classList.remove('bikes__nav-element_active');
+  desctopHighwayButton.classList.remove('bikes__nav-element_active');
+  desctopGravelButton.classList.add('bikes__nav-element_active');
+});
+
+desctopTtButton.addEventListener("click", function(){
+  bikesSliderWrapper.innerHTML = "";
+  bikesSliderWrapper.append(ttTemplate.content.cloneNode(true));
+  desctopHighwayButton.classList.remove('bikes__nav-element_active');
+  desctopGravelButton.classList.remove('bikes__nav-element_active');
+  desctopTtButton.classList.add('bikes__nav-element_active');
+});
+
